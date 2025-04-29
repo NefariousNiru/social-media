327263159da1fea02193626c4f9366c6baf38300
NefariousNiru <nirupomboseroy99@gmail.com>
2025-04-29 00:32:44 -0400

    Update: Versioning of AUTH Endpoints as V1

--- src/main/java/com/nefarious/socialnetwork/auth/util/AuthEndpoint.java
+++ src/main/java/com/nefarious/socialnetwork/auth/util/AuthEndpoint.java
@@ -1,13 +1,13 @@
 package com.nefarious.socialnetwork.auth.util;
 
 public class AuthEndpoint {
-    public static final String AUTH             = "/auth";
-    public static final String AUTH_SIGNUP      = "/signup";
-    public static final String AUTH_SIGNIN      = "/signin";
-    public static final String AUTH_VERIFY      = "/verify";
-    public static final String RESEND_OTP       = "/resend-otp";
-    public static final String REFRESH          = "/refresh";
-    public static final String LOGOUT           = "/logout";
-    public static final String FORGOT_PASSWORD  = "/forgot-password";
-    public static final String FORGOT_PASSWORD_VERIFY  = "/forgot-password/verify";
+    public static final String AUTH_V1                      = "/v1/auth";
+    public static final String AUTH_SIGNUP_V1               = "/v1/signup";
+    public static final String AUTH_SIGNIN_V1               = "/v1/signin";
+    public static final String AUTH_VERIFY_V1               = "/v1/verify";
+    public static final String RESEND_OTP_V1                = "/v1/resend-otp";
+    public static final String REFRESH_V1                   = "/v1/refresh";
+    public static final String LOGOUT_V1                    = "/v1/logout";
+    public static final String FORGOT_PASSWORD_V1           = "/v1/forgot-password";
+    public static final String FORGOT_PASSWORD_VERIFY_V1    = "/v1/forgot-password/verify";
 }
